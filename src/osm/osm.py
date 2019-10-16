4df860e899e1a866898015b5a13d98181130a770
jamc9597 <cunningham.844@osu.edu>
2019-10-15 23:01:23 -0400

    Fix typo on variable location_name

--- src/osm/osm.py
+++ src/osm/osm.py
@@ -2,7 +2,7 @@ import overpy
 
 def get_location_coords(location_name):
     api = overpy.Overpass()
-    result = api.query("relation[name=%s];out center;" % location)
+    result = api.query("relation[name=%s];out center;" % location_name)
     max_rel = result.relations[0]
     for rel in result.relations:
         if len(rel.members) > len(max_rel.members):
